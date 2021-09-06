60cb19fc523c9c1dd0b36991e83686e9dd5a9811
gaoto5454 <89964452+gaoto5454@users.noreply.github.com>
2021-09-06 12:15:30 +0900

    Update research_add.php

--- research_add.php
+++ research_add.php
@@ -9,12 +9,8 @@
 </head>
 
 <body>
-    
-<?php
-    echo "$_POST['name']";
-?>
 
-<!-- <?php
+<?php
 $number = $_POST['year'];
 if (!empty($number)) {
 $year = "after:" . "<" . $number . ">";
@@ -32,56 +28,56 @@ $backyear = '';
 ?>
     
 <form name="j_global" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　j-global　<?php echo $year; ?><?php echo $backyear; ?>">  <!--j-globalで検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name'];?>　j-global　<?php echo $year; ?><?php echo $backyear; ?>">  <!--j-globalで検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.j_global.submit();</SCRIPT>
 </form>
 
 <form name="astamuse" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　astamuse　<?php echo $year; ?><?php echo $backyear; ?>">  <!--astamuseで検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　astamuse　<?php echo $year; ?><?php echo $backyear; ?>">  <!--astamuseで検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.astamuse.submit();</SCRIPT>
 </form>
 
 <form name="ip_force" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　ip force　<?php echo $year; ?><?php echo $backyear; ?>">  <!--ip_forceで検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　ip force　<?php echo $year; ?><?php echo $backyear; ?>">  <!--ip_forceで検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.ip_force.submit();</SCRIPT>
 </form>
 
 <form name="address" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　<?php echo $_POST['address'] ?>　<?php echo $year; ?><?php echo $backyear; ?>"> <!--$_POST['address']で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　<?php echo $_POST['address']; ?>　<?php echo $year; ?><?php echo $backyear; ?>"> <!--$_POST['address']で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.address.submit();</SCRIPT>
 </form>
 
 <form name="PDF" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　PDF　<?php echo $year; ?><?php echo $backyear; ?><?php echo $backyear; ?>">  <!--PDFで検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　PDF　<?php echo $year; ?><?php echo $backyear; ?>">  <!--PDFで検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.PDF.submit();</SCRIPT>
 </form>
 
 <form name="teacher" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　教師　<?php echo $year; ?><?php echo $backyear; ?><?php echo $backyear; ?>">  <!--教師で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　教師　<?php echo $year; ?><?php echo $backyear; ?>">  <!--教師で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.teacher.submit();</SCRIPT>
 </form>
 
 <form name="pharmacist" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　薬剤師　<?php echo $year; ?><?php echo $backyear; ?>">  <!--薬剤師で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　薬剤師　<?php echo $year; ?><?php echo $backyear; ?>">  <!--薬剤師で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.pharmacist.submit();</SCRIPT>
 </form>
 
 <form name="JSDF" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　自衛隊　<?php echo $year; ?><?php echo $backyear; ?>">  <!--自衛隊で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　自衛隊　<?php echo $year; ?><?php echo $backyear; ?>">  <!--自衛隊で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.JSDF.submit();</SCRIPT>
@@ -95,28 +91,28 @@ $backyear = '';
 </form>
 
 <form name="patent" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　特許　<?php echo $year; ?><?php echo $backyear; ?>">  <!--特許で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　特許　<?php echo $year; ?><?php echo $backyear; ?>">  <!--特許で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.patent.submit();</SCRIPT>
 </form>
 
 <form name="plant" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　工場　<?php echo $year; ?><?php echo $backyear; ?>">  <!--工場で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　工場　<?php echo $year; ?><?php echo $backyear; ?>">  <!--工場で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.plant.submit();</SCRIPT>
 </form>
 
 <form name="government_worker" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　公務員　<?php echo $year; ?><?php echo $backyear; ?>">  <!--公務員で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　公務員　<?php echo $year; ?><?php echo $backyear; ?>">  <!--公務員で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.government_worker.submit();</SCRIPT>
 </form>
 
 <form name="move" method="get" action="https://www.google.co.jp/search" target="_blank">
-    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']?>　異動　<?php echo $year; ?><?php echo $backyear; ?>">  <!--異動で検索したい-->
+    <input type="text" name="q" size="31" maxlength="255" value="<?php echo $_POST['name']; ?>　異動　<?php echo $year; ?><?php echo $backyear; ?>">  <!--異動で検索したい-->
     <input type="submit" name="btng" value="検索">
     <input type="hidden" name="hl" value="ja">
     <SCRIPT language="JavaScript">document.move.submit();</SCRIPT>
@@ -126,6 +122,6 @@ $backyear = '';
 <form>
 <input type="button" onclick="history.back()" value="戻る">
 </form>
- -->
+    
 </body>
 </html>
